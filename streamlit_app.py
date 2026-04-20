b4b3fc3442a9c44a1dcd4a6a1d69a0073af509db
MatySantos <santos.matildedasilva@gmail.com>
2026-04-20 21:14:23 +0000

    Primeiro passo,

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Guia de Doses Pediátricas", layout="centered")
+st.title("Calculadora Doses Pediátricas")
+# 1. Escolha do Medicamento primeiro
+escolha = st.selectbox("Selecione o Medicamento:", ["Paracetamol 40 mg/mL", "Ibuprofeno 20 mg/mL","Ibuprofeno 40mg/mL"])
