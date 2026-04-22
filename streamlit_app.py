da755fd06bf523a2c104d27f2e65e579f9f8e894
MatySantos <santos.matildedasilva@gmail.com>
2026-04-22 22:15:37 +0000

    calhau

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,6 @@ elif escolha == "Amoxicilina + Ác. Clavulanico 250/64.5":
         st.metric (label= "Volume a tomar (mL)", value = f"{dose} mL" )
     elif peso >= 40:
         st.write ("Peso acima de 40 Kg, dose de adulto: comprimidos. Consultar médico")
-    if dose / 14 > 100:
+    if dose * 14 > 100:
         st.write ("Dose acima de 100 mL, necessários dois frascos.")
         
