4c88bd0b11e932d708b14c405fb0070d37868783
MatySantos <santos.matildedasilva@gmail.com>
2026-04-22 18:26:55 +0000

    correçao erro "

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="Guia de Doses Pediátricas", layout="centered")
 st.title("Calculadora Doses Pediátricas")
 # 1. Escolha do Medicamento primeiro
-escolha = st.selectbox("Selecione o Medicamento:", ["Selecione o Medicamento", Paracetamol 40 mg/mL", "Ibuprofeno 20 mg/mL","Ibuprofeno 40mg/mL", "Amoxicilina + Ác. Clavulanico 250/64.5", "Amoxicilina + Ác.Clavulanico 400/57", "Amoxicilina + Ác.Clavulanico 600/42.9", "Azitromicina 40 mg/mL"])
+escolha = st.selectbox("Selecione o Medicamento:", ["Selecione o Medicamento", "Paracetamol 40 mg/mL", "Ibuprofeno 20 mg/mL","Ibuprofeno 40mg/mL", "Amoxicilina + Ác. Clavulanico 250/64.5", "Amoxicilina + Ác.Clavulanico 400/57", "Amoxicilina + Ác.Clavulanico 600/42.9", "Azitromicina 40 mg/mL"])
 
 #2 Lógica específica de cada um 
 if escolha == "Paracetamol 40 mg/mL":
